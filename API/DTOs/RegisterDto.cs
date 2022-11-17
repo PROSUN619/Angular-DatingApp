13005e705ca7c234ce4c1fd8b7100bb24be77bd5
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-11-17 14:14:38 +0530

    Section 7 : Error Handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,6 +11,7 @@ namespace API.DTOs
         [Required]
         public string UserName { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
