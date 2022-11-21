7499a02d6f1108f56ea95bb75a166ea8359ad40a
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-11-21 19:36:40 +0530

    section 10 : updating resources

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -41,6 +42,18 @@ namespace API.Controllers
         {
             var user = await _repository.GetUserByUserNameAsync(username);
             return _mapper.Map<MemberDto>(user);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser (MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var user = await _repository.GetUserByUserNameAsync(username);
+            _mapper.Map(memberUpdateDto,user);
+            _repository.Update(user);
+            if (await _repository.SaveAllAsync()) return NoContent();
+            return BadRequest();
+
         }
     }
 }
---
+++ client/src/app/_guards/prevent-unsave-changes.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsaveChangesGuard implements CanDeactivate<unknown> {
+
+  canDeactivate(
+    component: MemberEditComponent): boolean {
+    if (component.editForm.dirty)
+      return confirm('Are you sure want to exit? Any unsaved change will be lost');
+    return true;
+  }
+
+}
